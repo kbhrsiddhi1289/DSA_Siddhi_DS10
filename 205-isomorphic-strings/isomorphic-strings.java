ee4146031429239a8391f11eb5484cb74fb57093
kbhrsiddhi1289 <kbhrsiddhi1289@gmail.com>
2025-10-31 20:04:31 +0530

    Time: 26 ms (7.96%) | Memory: 43.7 MB (5.06%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            // Check mapping s -> t
+            if (mapST.containsKey(c1) && mapST.get(c1) != c2)
+                return false;
+
+            // Check mapping t -> s (to avoid reverse conflicts)
+            if (mapTS.containsKey(c2) && mapTS.get(c2) != c1)
+                return false;
+
+            mapST.put(c1, c2);
+            mapTS.put(c2, c1);
+        }
+
+        return true;
+    }
+}
